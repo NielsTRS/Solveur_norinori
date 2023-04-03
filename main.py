a9e34763b1d371c39c2103cade6d3082c50b7023
NielsTRS <trsniels@gmail.com>
2023-04-03 10:50:13 +0200

    Added sat solver

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 import libs.rule as rule
 import libs.grid as grid
+from pysat.solvers import Glucose3
+from pysat.formula import CNF
 
-n = 2
+n = 3
 zone = 1
+name = "dimacs.cnf"
 
 grille = grid.Grid(n, zone)
 regle = rule.Rule(n)
@@ -12,6 +15,7 @@ regle = rule.Rule(n)
 print(grille.getGrid())
 
 grille.setCellValueColor(1, 1, 1)
+grille.setCellValueColor(3, 3, 1)
 
 regle.generateClauses(grille)
 print(regle.getClauses())
@@ -19,4 +23,26 @@ print(regle.getClauses())
 regle.filterClauses(grille)
 print(regle.getClauses())
 
-regle.generateDimacs("dimacs.txt")
+regle.generateDimacs(name)
+
+# sat solver
+cnf = CNF(from_file=name)  # reading from file
+
+solver = Glucose3()
+
+# Add the clauses to the solver
+solver.append_formula(cnf)
+
+# Solve the SAT problem
+solution = solver.solve()
+
+# Print the solution
+if solution:
+    model = solver.get_model()
+    print('Satisfiable')
+    print('Solution:', model)
+    for id in model:
+        if id > 0:
+            print(f"Il faut colorier la case de coordonnées : {grille.getCellIJById(id)}")
+else:
+    print('Non satisfiable')
